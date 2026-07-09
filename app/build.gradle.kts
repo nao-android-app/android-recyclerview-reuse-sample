6e1dc1e7612eef5767c832985b2fc0fd3978ae90
nao-android-app <rinrinpana@gmail.com>
2026-07-10 00:58:27 +0900

    fix: テーマ設定の誤りを修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.nao.view"
+        applicationId = "com.nao.recyclerviewreuse"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
--- app/src/androidTest/java/com/nao/recyclerviewreuse/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nao/recyclerviewreuse/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.nao.view", appContext.packageName)
+        assertEquals("com.nao.recyclerviewreuse", appContext.packageName)
     }
 }

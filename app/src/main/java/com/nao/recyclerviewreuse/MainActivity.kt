de1ae181a8d9347f8fd8b3457c3d24cbc08c3e2a
nao-android-app <rinrinpana@gmail.com>
2026-07-19 00:14:15 +0900

    fix: システムバーとの重なりを防ぐためWindowInsetsを適用

--- app/src/main/java/com/nao/recyclerviewreuse/MainActivity.kt
+++ app/src/main/java/com/nao/recyclerviewreuse/MainActivity.kt
@@ -3,6 +3,9 @@ package com.nao.recyclerviewreuse
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.nao.recyclerviewreuse.databinding.ActivityMainBinding
 import com.nao.recyclerviewreuse.model.Item
@@ -13,15 +16,22 @@ import com.nao.recyclerviewreuse.model.Item
 private val sampleMode = SampleMode.FIXED
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { view, insets ->
+            val bars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.updatePadding(
+                top = bars.top,
+                bottom = bars.bottom
+            )
+            insets
+        }
 
         binding.recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
